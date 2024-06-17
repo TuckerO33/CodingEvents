35b698c4925885b614d94c2543b7827c77c7fe27
Tucker Overmann <tuckerovermann@gmail.com>
2024-06-17 10:09:54 -0500

    did most of the studio, just pushing so I can switch branches

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using CodingEvents.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CodingEvents.Controllers
 {
+    [Route("/eventcategory")]
     public class EventCategoryController : Controller
     {
         private EventDbContext context;
@@ -25,6 +28,29 @@ namespace CodingEvents.Controllers
             List<EventCategory> categories = context.Categories.ToList();
             return View(categories);
 
+        }
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+
+            AddEventCategoryViewModel addEventCategoryViewModel = new();
+            if (ModelState.IsValid)
+            {
+                EventCategory newEventCategoty = new EventCategory
+                {
+                    Name = addEventViewModel.Name,
+                };
+
+                context.EventCategory.Add(newEventCategoty);
+                context.SaveChanges();
+            }
+
+            return View(addEventViewModel);
+        }
+
+        public IActionResult ProcessCreateEventCategoryForm()
+        {
+            return View();
         }
     }
 }
